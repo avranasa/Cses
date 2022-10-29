22dc44b7f4940ae9e2bc0a7bf89b203c6c5305fb
avranasa <46421575+avranasa@users.noreply.github.com>
2022-10-29 16:46:31 +0200

    Update 18_PlanetsQueriesI.cpp

--- 4_GraphAlgorithms/18_PlanetsQueriesI.cpp
+++ 4_GraphAlgorithms/18_PlanetsQueriesI.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int main(){    
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, q, t, x, k, F=30, Method=2;
+    ios::sync_with_stdio(0);//those two commands are very important. It is much slower without them.
+    cin.tie(0);           //Especially if you immediately "cout" the correct answer instead of first
+                //gathering all the answers in the ans[q] array and at the end "cout" them all together
+
+    int n, q, t, x, k, F=30, Method=1;
     cin >> n >> q;
     int Tele[F][n], ans[q]; //the (i,j) element represents the planet you land after 2**(i+1) steps 
                     //starting from j-th planet...2^F>10^9 so 29 steps are enough. 
@@ -37,9 +39,9 @@ int main(){
             //if (Sink[x]) break;
             t++; k /= 2;
             }
-        }     
-        ans[i] = x+1;
-        //cout << x+1 << endl; //slower (0.05sec) than gathering all the answers and after doing the "cout"(outside the loop.)
+        }
+        cout << x+1 << '\n'; ///endl is much SLOWER (0.3 sec)        
+        //ans[i] = x+1; 
     }
-    for(int i=0; i<q; i++) cout << ans[i] << endl; 
+    //for (int i=0;i<q;i++) cout << ans[i] << '\n';
 }
